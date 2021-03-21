247e174cde4b01c6bb5083712aae2a627927f24e
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-03-21 12:06:40 -0500

    Navigating with buttons...
    Point out using string URL, but can be more "complex" to satisfy more complex requirements ! ! !

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-play',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  winGame() {
+    this.router.navigateByUrl("/");
+  }
+
+  loseGame() {
+    this.router.navigateByUrl("/");
+  }
 }
