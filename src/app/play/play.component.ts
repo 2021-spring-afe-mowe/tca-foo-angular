cdb78f9ed9b0c7c7b29fee21f9c64733bfedeb84
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-03-21 12:12:01 -0500

    Adding wins and losses to app data...

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-play',
@@ -10,16 +11,25 @@ export class PlayComponent implements OnInit {
 
   constructor(
     private router: Router
+    , private appDataSvc: AppService
   ) { }
 
   ngOnInit(): void {
   }
 
   winGame() {
+    this.appDataSvc.gameResults = [
+      ...this.appDataSvc.gameResults
+      , "W"
+    ];
     this.router.navigateByUrl("/");
   }
 
   loseGame() {
+    this.appDataSvc.gameResults = [
+      ...this.appDataSvc.gameResults
+      , "L"
+    ];
     this.router.navigateByUrl("/");
   }
 }
