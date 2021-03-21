511ea2d7c3b1fd919b7aa496688e94783a2c6659
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-03-21 12:40:05 -0500

    timeConversion from tca-uno, from stack overflow...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,13 +20,38 @@ export class HomeComponent implements OnInit {
     this.calculateGameLengthStats();
   }
 
+  timeConversion(duration: number) {
+    const portions: string[] = [];
+  
+    const msInHour = 1000 * 60 * 60;
+    const hours = Math.trunc(duration / msInHour);
+    if (hours > 0) {
+      portions.push(hours + 'h');
+      duration = duration - (hours * msInHour);
+    }
+  
+    const msInMinute = 1000 * 60;
+    const minutes = Math.trunc(duration / msInMinute);
+    if (minutes > 0) {
+      portions.push(minutes + 'm');
+      duration = duration - (minutes * msInMinute);
+    }
+  
+    const seconds = Math.trunc(duration / 1000);
+    if (seconds > 0) {
+      portions.push(seconds + 's');
+    }
+  
+    return portions.join(' ');
+  }
+
   calculateGameLengthStats() {
     const longest = Math.max(...this.appDataSvc.gameResults.map(x => x.endDateTime - x.startDateTime));
     const shortest = Math.min(...this.appDataSvc.gameResults.map(x => x.endDateTime - x.startDateTime));
 
     this.gameLengthStats = {
-      longest: isFinite(longest) ? longest : 0 
-      , shortest: isFinite(shortest) ? shortest : 0
+      longest: isFinite(longest) ? this.timeConversion(longest) : "n/a" 
+      , shortest: isFinite(shortest) ? this.timeConversion(shortest) : "n/a"
     };
   }
 
