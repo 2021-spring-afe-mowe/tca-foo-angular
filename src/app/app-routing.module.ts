6b01ba807023b89b0cafb2421a6b3548513cac7e
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-03-22 15:11:14 -0500

    Routing working...
    At least by hacking the browser address ! ! !

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlayComponent } from './play/play.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ''
+    , component: HomeComponent
+  }
+  , {
+    path: 'play'
+    , component: PlayComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
